ab1f9a14863eae2c2094cd5cd601510bb3382fcb
Bob Jin <bobbay@BobMacbookAir.local>
2019-11-26 17:18:48 +1100

    update main.py to run all scripts

--- jinfund/__main__.py
+++ jinfund/__main__.py
@@ -1,4 +1,17 @@
-from . import app
+# Standard library imports
 
-if __name__ == '__main__':
-    app.run()
+
+# Local imports
+from analysis import lookthru
+from data import etl, download
+
+# Get the data frames
+blackrock_df, vanguard_df = etl.etl_preprocessing()
+
+# Update data
+download.blackrock()
+download.vanguard()
+
+# Pass the weights through after instantiating lookthru
+lookthru = lookthru.analysis(blackrock_df, vanguard_df)
+blackrock_df, vanguard_df = lookthru.pass_weights()
--- jinfund/app.py
+++
@@ -1,9 +0,0 @@
-from analysis import lookthru
-from data import etl
-
-# Get the data frames
-blackrock_df, vanguard_df = etl.etl_preprocessing()
-
-# Pass the weights through after instantiating lookthru
-lookthru = lookthru.analysis(blackrock_df, vanguard_df)
-blackrock_df, vanguard_df = lookthru.pass_weights()
