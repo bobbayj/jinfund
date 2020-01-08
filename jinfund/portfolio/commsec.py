481cee6896258cc085f16e95496e17adbefbde08
jin-and-tonic <35100300+jindustries@users.noreply.github.com>
2020-01-09 00:54:10 +1100

    update commsec pseudocode

--- jinfund/portfolio/commsec.py
+++ jinfund/portfolio/commsec.py
@@ -2,20 +2,28 @@
 Reads and prepares Commsec transaction.csv files
 '''
 
-# Structure as 
+# Structure as Class object
 
-# Read csv. Note that transactions only show on the T+2 date
+# init
+    # Read csv. Note that transactions only show on the T+2 date
 
-# Split details by space
-    # If details[0] not in ['B','S'], skip delete row
+    # Split details by space
+        # If details[0] not in ['B','S'], skip delete row
 
-# Create dataframe
-    # Index = Date
-    # Columns = type | asxCode | volume | debit | credit | orderPrice
-# Update price to include brokerage
-    # price = debit or credit / volume
-    # Add price as column, remove debit and credit cols
-    # Add brokerage as column: int(abs(price/orderPrice-1)*100), remove orderPrice col
+    # Create dataframe
+        # Index = Date
+        # Columns = type | asxCode | volume | debit | credit | orderPrice
+    # Update price to include brokerage
+        # price = debit or credit / volume
+        # Add price as column
+        # combine debit and credit cols as a single col: value
+        # Add brokerBps as column: int(abs(price/orderPrice-1)*100), remove orderPrice col
+    
+# Methods (rtn functions)
+    # final dataframe
+    # buys only
+    # sells only
+    # total brokerage cost (skip)
 
 # -------
 # Testing script
