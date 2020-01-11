6763cd6f71a05d3def3da8b53fac52876eed6e3f
jin-and-tonic <35100300+jindustries@users.noreply.github.com>
2020-01-12 01:30:31 +1100

    pseudocode to update portfolio holdings as df

--- jinfund/portfolio.py
+++ jinfund/portfolio.py
@@ -26,61 +26,77 @@ class Portfolio:
         Returns:
             DataFrame -- index = datetime.date | columns = tickers | values = {'vol', 'vwap'}
         '''
-        
-        portfolio_holdings = {key: None for key in self.portfolio_dates}
-        portfolio_df = add_divs_splits(portfolio_df)
-
         trade_dates = self.df_txs.index.to_list()
         trade_tickers = self.df_txs.Ticker.to_list()
         trade_types = self.df_txs.TradeType.to_list()
         trade_volumes = self.df_txs.Volume.to_list()
         trade_prices = self.df_txs.EffectivePrice.to_list()
+        
+        # portfolio_holdings = {key: None for key in self.portfolio_dates}
+        portfolio_df = pd.DataFrame(index=self.portfolio_dates, columns=['cash', trade_tickers])
+        portfolio_df = add_divs_splits(portfolio_df)
 
-        holdings = {
-            'cash':0,
-            }
+
+        # holdings = {
+        #     'cash':0,
+        #     }
 
         trade_date = trade_dates.pop()
-        for portfolio_date in sorted(self.portfolio_dates, reverse=False):
-            while trade_date == portfolio_date:
-                trade_ticker = trade_tickers.pop()
-                trade_type = trade_types.pop()
-                trade_vol = trade_volumes.pop()
-                trade_price = trade_prices.pop()
 
-                if trade_ticker in holdings:
-                    if trade_type == 'B':  # Trade is a buy
-                        new_vol = holdings[trade_ticker]['vol'] + trade_vol
-                        new_vwap = (
-                            (holdings[trade_ticker]['vol']*holdings[trade_ticker]['vwap']
-                            + trade_vol*trade_price) / new_vol
-                        )
-                        holdings[trade_ticker] = {
-                            'vol': new_vol,
-                            'vwap': np.round(new_vwap, decimals=3),
-                            }
-                        holdings['cash'] -= np.round(trade_vol*trade_price, decimals=3)
-                    else:  # Trade is a sale
-                        holdings[trade_ticker]['vol'] -= trade_vol
-                        holdings['cash'] += np.round(trade_vol*trade_price, decimals=3)
-                else:
-                    holdings[trade_ticker] = {
-                        'vol': trade_vol,
-                        'vwap': np.round(trade_price, decimals=3),
-                    }
-                    holdings['cash'] -= np.round(trade_vol*trade_price, decimals=3)
+        for trade_date in trade_dates:
+            trade_ticker = trade_tickers.pop()
+            trade_type = trade_types.pop()
+            trade_vol = trade_volumes.pop()
+            trade_price = trade_prices.pop()
+
+            # portfolio_df.at[trade_date,trade_ticker]
+
+            ### TO-DO
+            # Dot in the trades in addition to the dividends and splits
+            # Backfill data to today
+            # Function: use trade_date and trade_date-1 to update trade_date holdings (incl. stock split)
+            ### END TO-DO
+
+        # for portfolio_date in sorted(self.portfolio_dates, reverse=False):
+        #     while trade_date == portfolio_date:
+        #         trade_ticker = trade_tickers.pop()
+        #         trade_type = trade_types.pop()
+        #         trade_vol = trade_volumes.pop()
+        #         trade_price = trade_prices.pop()
+
+        #         if trade_ticker in holdings:
+        #             if trade_type == 'B':  # Trade is a buy
+        #                 new_vol = holdings[trade_ticker]['vol'] + trade_vol
+        #                 new_vwap = (
+        #                     (holdings[trade_ticker]['vol']*holdings[trade_ticker]['vwap']
+        #                     + trade_vol*trade_price) / new_vol
+        #                 )
+        #                 holdings[trade_ticker] = {
+        #                     'vol': new_vol,
+        #                     'vwap': np.round(new_vwap, decimals=3),
+        #                     }
+        #                 holdings['cash'] -= np.round(trade_vol*trade_price, decimals=3)
+        #             else:  # Trade is a sale
+        #                 holdings[trade_ticker]['vol'] -= trade_vol
+        #                 holdings['cash'] += np.round(trade_vol*trade_price, decimals=3)
+        #         else:
+        #             holdings[trade_ticker] = {
+        #                 'vol': trade_vol,
+        #                 'vwap': np.round(trade_price, decimals=3),
+        #             }
+        #             holdings['cash'] -= np.round(trade_vol*trade_price, decimals=3)
                 
-                if len(trade_dates) > 0:
-                    trade_date = trade_dates.pop()
-                else:
-                    break
-            portfolio_holdings[portfolio_date] = copy.deepcopy(holdings)  # To preserve the mutability of each daily holding
+        #         if len(trade_dates) > 0:
+        #             trade_date = trade_dates.pop()
+        #         else:
+        #             break
+        #     portfolio_holdings[portfolio_date] = copy.deepcopy(holdings)  # To preserve the mutability of each daily holding
 
-        portfolio_df = pd.DataFrame.from_dict(
-            portfolio_holdings,
-            orient='index'
-            )
-        portfolio_df.index.names = ['Date']
+        # portfolio_df = pd.DataFrame.from_dict(
+        #     portfolio_holdings,
+        #     orient='index'
+        #     )
+        # portfolio_df.index.names = ['Date']
 
         return portfolio_df
 
