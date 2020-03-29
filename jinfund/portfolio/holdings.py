9695e5da6e93940c344dddffef146f2f8039cbd5
jin-and-tonic <35100300+jindustries@users.noreply.github.com>
2020-03-30 00:46:39 +1100

    create plots of portfolio value and cost

--- jinfund/portfolio/holdings.py
+++ jinfund/portfolio/holdings.py
@@ -22,16 +22,22 @@ class Portfolio:
         self.portfolio_csv_name = f'portfolio_holdings.csv'
         self.portfolio_csv_path = self.save_dir / self.portfolio_csv_name
         
+        # Initialise trade transaction data
+        self.t = Trades()
+        self.trades = self.t.all
+
         # Create portfolio from trades
         self.holdings = self.build()
 
     def build(self):
-        # Initialise transaction data and get trades
-        trades = Trades()
-        trades_df = trades.all
+        '''Builds portfolio holdings from transaction data
+        
+        Returns:
+            Dataframe -- (Date, Ticker): [Volume, Close]
+        '''        
 
         # Construct range of dates over portfolio period
-        p_inception = trades_df.index[0][0]
+        p_inception = self.trades.index[0][0]
         p_dates = datehandler.date_list(p_inception, self.today)
 
         # Create base dataframe
@@ -41,7 +47,7 @@ class Portfolio:
         df_pfolio = pd.DataFrame()  # Initialise the main portfolio df as empty
 
         # Build list of tickers seen throughout investment period
-        tickers = list(sorted(set(trades_df.reset_index().Ticker.to_list())))
+        tickers = list(sorted(set(self.trades.reset_index().Ticker.to_list())))
 
         # Use yfinance to get close prices
         lookup_tickers = [f'{ticker}.AX' for ticker in tickers]  # Only supports ASX stocks
@@ -53,7 +59,7 @@ class Portfolio:
 
         # Step through transactions dataframe, ticker by ticker
         for ticker in tickers:
-            vol_df = trades.by_ticker(ticker)['Volume']  # Get volumes
+            vol_df = self.t.by_ticker(ticker)['Volume']  # Get volumes
 
             # Merge with base_dates_df and cumulative sum the volumes
             ticker_df = pd.merge_ordered(vol_df, df_base_dates, on='Date').set_index('Date')
@@ -69,7 +75,22 @@ class Portfolio:
             df_pfolio = pd.concat([df_pfolio, ticker_df])
 
         df_pfolio = df_pfolio.reset_index().set_index(['Date','Ticker']).sort_index() # Finalise portfolio
+        df_pfolio = df_pfolio.assign(Value = lambda x: x['Volume'] * x['Close'])
 
         return df_pfolio
     
-    def 
+    def plot(self, view='default'):
+        # Portfolio value
+        value_df = self.holdings.groupby('Date').sum()['Value']
+        value_df = value_df[value_df > 0]
+
+        # The portfolio cost for equity
+        cost_df = self.trades.assign(Value = lambda x: x['Volume'] * x['EffectivePrice'])
+        cost_df = cost_df.groupby('Date').sum()['Value'].cumsum()
+
+        plot_df = pd.merge_ordered(value_df, cost_df, on='Date', fill_method='ffill').set_index('Date')
+        plot_df.columns = ['Value', 'Cost']
+        plot_df = plot_df.assign(pl_line = lambda x: x['Value'] - x['Cost'])
+
+        if view == 'default':
+            plot_df.plot()
