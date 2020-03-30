4d885cf66344984bbabd62ea15788f92e6e5a14e
jin-and-tonic <35100300+jindustries@users.noreply.github.com>
2020-03-30 12:07:49 +1100

    create portfolio value and pl plotting function

--- jinfund/portfolio/holdings.py
+++ jinfund/portfolio/holdings.py
@@ -66,6 +66,9 @@ class Portfolio:
             ticker_df = ticker_df.cumsum().ffill()
             ticker_df = ticker_df[ticker_df['Volume'].isna() == False]
 
+            # Update volumes based on stocksplits
+            ticker_df = self.stocksplits(ticker_df,ticker)
+
             # Merge with close prices
             ticker_df = pd.merge_ordered(ticker_df, close[ticker], on='Date').set_index('Date')
             ticker_df.rename({ticker: 'Close'}, axis=1, inplace=True)
@@ -74,11 +77,54 @@ class Portfolio:
             ticker_df['Ticker'] = ticker # create ticker column for later indexing
             df_pfolio = pd.concat([df_pfolio, ticker_df])
 
-        df_pfolio = df_pfolio.reset_index().set_index(['Date','Ticker']).sort_index() # Finalise portfolio
-        df_pfolio = df_pfolio.assign(Value = lambda x: x['Volume'] * x['Close'])
+        df_pfolio = df_pfolio.reset_index().set_index(['Date','Ticker']).sort_index()  # Finalise portfolio
+        df_pfolio = df_pfolio.assign(Value = lambda x: x['Volume'] * x['Close'])  # Calculate portfolio value
+        print('\nPortfolio successfully built')
 
         return df_pfolio
-    
+
+    def stocksplits(self, df, ticker):
+        '''Adjusts volumes according to historical stocksplits
+        
+        Arguments:
+            df {DataFrame} -- Dataframe with date (index) and volume (series) data
+            ticker {String} -- Ticker to explore. Only ASX securities supported
+        
+        Returns:
+            DataFrame -- Returns dataframe, but with volume scaled by the cumulative stocksplits
+        '''
+        # Terminal output
+        print(f'\rChecking {ticker}', end='', flush=True)
+
+        try:  # Dividends and stock splits
+            stock = yf.Ticker(f'{ticker}.AX')
+            df_actions = stock.actions
+        except TypeError:
+            print(f'\rSomething went wrong!',flush=True)
+            return df
+        
+        # Add stocksplits column
+        if len(df_actions[df_actions['Stock Splits'] > 0]) > 0:
+            df['Stock Splits'] = df_actions['Stock Splits'].replace(0,np.nan)
+
+            # Set all dates with 0 holdings to have np.nan holdings
+            df['Volume'] = df['Volume'].replace(0,np.nan)
+
+            stocksplit_cumulative = df.loc[df['Volume'].first_valid_index():]['Stock Splits'].cumprod()
+            stocksplit_cumulative = stocksplit_cumulative.fillna(method='ffill').fillna(1)
+
+
+            # Update holding volume
+            df['Volume'] *= stocksplit_cumulative
+            df['Volume'] = np.ceil(df['Volume'])
+            
+            # Update vwap
+            # df['Vwap'] /= stocksplit_cumulative
+
+            df = df.drop(columns='Stock Splits')  # Finalise dataframe to return by removing excess columns
+
+        return df
+
     def plot(self, view='default'):
         # Portfolio value
         value_df = self.holdings.groupby('Date').sum()['Value']
@@ -91,6 +137,15 @@ class Portfolio:
         plot_df = pd.merge_ordered(value_df, cost_df, on='Date', fill_method='ffill').set_index('Date')
         plot_df.columns = ['Value', 'Cost']
         plot_df = plot_df.assign(pl_line = lambda x: x['Value'] - x['Cost'])
+        plot_df = plot_df.assign(pl_pct = lambda x: x['pl_line']/x['Cost'])
 
         if view == 'default':
-            plot_df.plot()
+            cols = ['Value','Cost']
+        elif view == 'pl':
+            cols = ['pl_line']
+        elif view == 'pl_pct':
+            cols = ['pl_pct']
+        
+        plot_df[cols].plot()
+
+        return plot_df
