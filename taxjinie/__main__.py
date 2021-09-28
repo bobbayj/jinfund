9d34b92f06901410977ddba3bafa859d7d55764b
BobbayJ <private>
2021-09-29 01:23:49 +1000

    export performance reports for further analysis

--- taxjinie/__main__.py
+++ taxjinie/__main__.py
@@ -1,15 +1,18 @@
 from transactions import tx_loader
-from analysis import tax
+from analysis import tax, performance
 
 def main():
     commsec = tx_loader.Loader()
     commsec.build()
 
-    tax_reporting = tax.Tax(2021)
-    tax_reporting.capital_gain_events()
-    tax_reporting.upcoming_cgtdiscounts()
-    tax_reporting.cgt_report(output_type='excel')
-    tax_reporting.export_tx_history()
+    # tax_reporting = tax.Tax(2021)
+    # tax_reporting.capital_gain_events()
+    # tax_reporting.upcoming_cgtdiscounts()
+    # tax_reporting.cgt_report(output_type='excel')
+    # tax_reporting.export_tx_history()
+
+    performance_reporting = performance.Performance()
+    performance_reporting.monthly_cashflows(export=True)
 
 if __name__ == '__main__':
     main()
--- taxjinie/analysis/performance.py
+++ taxjinie/analysis/performance.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from datetime import datetime
+from pathlib import Path
 
 # Local imports
 from . import portfolio
@@ -7,32 +7,41 @@ from . import portfolio
 class Performance():
   def __init__(self) -> None:
     self.txs = portfolio.transactions()
-    self.cashflows = self.calculate_cashflows()
-    
-    inception_date = self.transactions.first_valid_index().date()
-    latest_month = datetime.today().date()
-    portfolio_months = pd.date_range(start=inception_date, end=latest_month, freq='MS')
-    self.monthly_dataframe = pd.DataFrame(portfolio_months, columns=['Month']).set_index('Month')
+    self.calculate_tx_cashflows()
   
-  def calculate_cashflows(self):
+  def calculate_tx_cashflows(self):
     # Group transactions into months
     self.txs['CashflowIncBrokerage'] = self.txs['Volume'] * self.txs['PriceIncBrokerage']
     self.txs['Cashflow'] = self.txs['Volume'] * self.txs['Price']
   
-  def ticker_monthly_summary(self, ticker:str):
+  def _ticker_monthly_cashflows(self, ticker):
     ticker_df = self.txs[self.txs['Ticker'] == ticker]
     ticker_df = ticker_df.groupby(pd.Grouper(freq='M')).sum()
+    ticker_df = ticker_df.set_index(ticker_df.index.values.astype('datetime64[M]'))
+    
+    ticker_df['Ticker'] = ticker
+    ticker_series = ticker_df['Ticker']
+    ticker_df = ticker_df.drop(columns=['Ticker'])
+    ticker_df.insert(loc=0, column='Ticker', value=ticker_series)
     
     return ticker_df[ticker_df['Volume'] != 0]
 
-  def money_weight_return(self, ticker:str='portfolio'):
+  def monthly_cashflows(self, ticker:str='portfolio', export=False):
     ticker_df = pd.DataFrame()
 
     if ticker == 'portfolio':
-      for ticker in self.txs['Ticker'].unique():
-        ticker_df.append(self.ticker_monthly_summary(ticker))
+      for looped_ticker in self.txs['Ticker'].unique():
+        ticker_df = ticker_df.append(self._ticker_monthly_cashflows(looped_ticker))
 
     else:
-      ticker_df.append(self.ticker_monthly_summary(ticker))
+      ticker_df = ticker_df.append(self._ticker_monthly_cashflows(ticker))
 
+    if export:
+      last_month = ticker_df.last_valid_index()
+      fname = f'monthly_cashflows_{last_month:%Y%m%d}'
+      fpath = Path(__file__).parent.parent / 'reports' / fname
+      ticker_df.to_excel(fpath.with_suffix('.xlsx'))
 
+      print(f'Saved!\n\tFilename:\t{fpath.name}\n\tOutput path:\t{fpath}')
+
+    return ticker_df
